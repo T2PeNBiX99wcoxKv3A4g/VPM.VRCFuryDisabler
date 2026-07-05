7e6cbac9b83624282bd8e7347ed2888a38239e20
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-07-06 02:35:10 +0800

    chore: Added VPM Resolver and VRCFury Disabler packages; implemented package management and asset migration tools along with associated data handling and editor utilities.

---
+++ Packages/io.github.ykysnk.vrcfury-disabler/Editor/VRCFuryPatches.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using io.github.ykysnk.utils;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+
+namespace io.github.ykysnk.VRCFuryDisabler.Editor
+{
+    [InitializeOnLoad]
+    internal static class VRCFuryPatches
+    {
+        private const string PatchId = "io.github.ykysnk.vrcfury-disabler.patches";
+        private static readonly Type ThisType = typeof(VRCFuryPatches);
+        private static readonly Type VRCFProgressWindowType = AccessTools.TypeByName("VF.VRCFProgressWindow");
+        private static readonly Type VRCFuryBuilderType = AccessTools.TypeByName("VF.Builder.VRCFuryBuilder");
+        private static readonly Type EditorWindowType = typeof(EditorWindow);
+
+        private static readonly MethodInfo CreateMethod = AccessTools.Method(VRCFProgressWindowType, "Create");
+
+        static VRCFuryPatches()
+        {
+            var harmony = new Harmony(PatchId);
+
+            harmony.PatchAll(ThisType.Assembly);
+
+            AssemblyReloadEvents.beforeAssemblyReload += () => harmony.UnpatchAll(PatchId);
+            Utils.Log(nameof(VRCFuryPatches), "VRCFury Disabler Patches Initialized");
+        }
+
+        private static void ProgressReplace([CanBeNull] object _, float current, string info)
+        {
+            var progressValue = Mathf.Clamp01(current);
+            var percent = Math.Round(progressValue * 100);
+            Utils.Log(nameof(VRCFuryPatches), $"Progress ({percent}%): {info}");
+            EditorUtility.DisplayProgressBar($"VRCFury Building {percent}%", info, progressValue);
+        }
+
+        private static void CloseReplace([CanBeNull] object _)
+        {
+            EditorUtility.ClearProgressBar();
+        }
+
+        [HarmonyPatch]
+        [SuppressMessage("ReSharper", "UnusedType.Global")]
+        [SuppressMessage("ReSharper", "UnusedMember.Global")]
+        public static class VRCFuryBuilderRun
+        {
+            private static readonly MethodInfo Method = AccessTools.Method(VRCFuryBuilderType, "Run");
+            private static readonly MethodInfo CloseMethod = AccessTools.Method(EditorWindowType, nameof(EditorWindow.Close));
+            private static readonly MethodInfo CloseReplaceMethod = AccessTools.Method(ThisType, nameof(CloseReplace));
+
+            public static MethodBase TargetMethod() => Method;
+
+            [HarmonyTranspiler]
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                foreach (var code in instructions)
+                {
+                    if (code.opcode == OpCodes.Call && (MethodInfo)code.operand == CreateMethod)
+                    {
+                        code.opcode = OpCodes.Ldnull;
+                        code.operand = null;
+                    }
+                    else if (code.opcode == OpCodes.Callvirt && (MethodInfo)code.operand == CloseMethod)
+                    {
+                        code.opcode = OpCodes.Call;
+                        code.operand = CloseReplaceMethod;
+                    }
+
+                    yield return code;
+                }
+            }
+        }
+
+        [HarmonyPatch]
+        [SuppressMessage("ReSharper", "UnusedType.Global")]
+        [SuppressMessage("ReSharper", "UnusedMember.Global")]
+        public static class VRCFuryBuilderApplyFuryConfigs
+        {
+            private static readonly MethodInfo Method = AccessTools.Method(VRCFuryBuilderType, "ApplyFuryConfigs");
+            private static readonly MethodInfo ProgressMethod = AccessTools.Method(VRCFProgressWindowType, "Progress");
+
+            private static readonly MethodInfo ProgressReplaceMethod =
+                AccessTools.Method(ThisType, nameof(ProgressReplace));
+
+            public static MethodBase TargetMethod() => Method;
+
+            [HarmonyTranspiler]
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                foreach (var code in instructions)
+                {
+                    if (code.opcode == OpCodes.Callvirt && (MethodInfo)code.operand == ProgressMethod)
+                    {
+                        code.opcode = OpCodes.Call;
+                        code.operand = ProgressReplaceMethod;
+                    }
+
+                    yield return code;
+                }
+            }
+        }
+    }
+}
---
+++ Packages/io.github.ykysnk.vrcfury-disabler/README.md
@@ -0,0 +1,4 @@
+# VRCFury Disabler
+
+Disables hidden features you didn't ask for.  
+Currently, this only replaces custom editor progress windows with the default progress bar.
